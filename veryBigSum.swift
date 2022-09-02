ad0eaf20f55584bc484ddff3cd08614c8cad5997
yisuax <vintique37@gmail.com>
2022-09-02 17:08:21 -0500

    Big Sum Challenge

---
+++ veryBigSum.swift
@@ -0,0 +1,39 @@
+/*
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+
+long: the sum of all array elements
+Input Format
+
+The first line of the input consists of an integer .
+The next line contains  space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+*/
+
+import Foundation
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    var conta = 0
+    for arry in ar {   
+      conta += arry
+    }
+    return conta
+}
